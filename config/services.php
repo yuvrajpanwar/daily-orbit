cb2bcf16eeac2dfba3d725818ac015ba8c523b47
yuvraj singh panwar <yogipanwar173@gmail.com>
2026-04-19 02:52:36 +0530

    indexing

--- config/services.php
+++ config/services.php
@@ -18,6 +18,10 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
+    'indexnow' => [
+        'key' => env('BING_INDEXNOW_KEY', ''),
+    ],
+
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
